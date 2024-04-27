f4bc1fdf1498c649ea41fd3783d787cdf4b57d68
Rocky0204 <135044945+Rocky0204@users.noreply.github.com>
2024-04-27 22:40:28 +0530

    dsa_lab_2

--- dsa_lab_2/stack-on-array.c
+++ dsa_lab_2/stack-on-array.c
@@ -1,4 +1,6 @@
 /*
+NAME: SWADHA SWAROOP
+ROLL: 112201009
 	Course:		CS2024 DSA Lab 
 	Semester:	2024 Jan-Apr
 	Lab:		02
@@ -10,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>	// Needed for malloc, free etc
+#include  <string.h>
 
 typedef struct {
 	char* data;	// Pointer to the start of array. Space will be allocated by init()
@@ -33,18 +36,31 @@ void init(Stack* s, int size) {
 		- On success: returns 0
 		- On error: Message to stderr and returns -1
 */
+// int push(Stack* s, char c) {
+// 	if (s->top >= s->size) {
+// 		fprintf(stderr, "Stack Overflow\n");
+// 		return -1;
+// 	}
+// 	s->data[s->top++]=c;
+// 	/* Task 1a.
+// 		Fill in the statements to add c to the stack and increment top	
+// 	*/
+// 	return 0;
+// }
 int push(Stack* s, char c) {
+	if (s->top >= s->size) {
+		s->data= (char*) realloc(s->data,2* s->size * sizeof(char));
+		s->size*=2;
+		printf("new size is : %d \n",s->size);
+	}
 	if (s->top >= s->size) {
 		fprintf(stderr, "Stack Overflow\n");
 		return -1;
 	}
-
-	/* Task 1a.
-		Fill in the statements to add c to the stack and increment top	
-	*/
-	
-
-	return 0;
+	else{
+		s->data[s->top++]=c;
+		return 0;
+		}	
 }
 
 /* 	### Pop function 
@@ -52,24 +68,65 @@ int push(Stack* s, char c) {
 		- On success: returns the character on top of stack
 		- On error: Message to stderr and returns '$'
 */
-char pop(Stack *s) {
+// char pop(Stack *s) {
+// 	char k;
+// 	if (s->top <= 0) {
+// 		fprintf(stderr, "Stack Empty\n");
+// 		return '$';
+// 	}
+// 	else{
+// 		k=s->data[--s->top];
+// 		return k;
+// 	}
+	/* Task 1b.
+		Fill in the missing statements 
+	*/
+// }
+
+    char pop(Stack *s) {
 	if (s->top <= 0) {
 		fprintf(stderr, "Stack Empty\n");
 		return '$';
 	}
-	/* Task 1b.
-		Fill in the missing statements 
-	*/
-	
+	if ((s->top == s->size/2) ){
+		s->data= (char*) realloc(s->data,(s->size * sizeof(char))/2);
+		s->size/=2;
+		printf("size of stack is : %d \n",s->size);
+	}
+	return s->data[--s->top];
+	}
 
+int checkingparanthesis(Stack* r,char a[]){
+	int sizeofstring = strlen(a);
+	if (sizeofstring%2 ==1){
+		return 0;
+	}
+    for(int i=0;i<sizeofstring;i++){
+		if (a[i] =='('){
+		push(r,a[i]);
+		}
+		else if(a[i] == ')'){
+			pop(r);
+		}
+	}
+	if(r->top==0){
+		return 1;
+	}
+	else{
+	return 0;
+	}
 }
-	
+
 int main() {
 	Stack s;
+	Stack r;
 	int i, flag;
 	char c;
-
 	init(&s, 16);
+	init(&r, 16);
+	char a[]="((()";
+	int b=checkingparanthesis(&r,a);
+    printf("%d \n",b);
 
 	for(i = 0; i < 26; i++) {
 		flag = push(&s, 'a'+i);
@@ -77,7 +134,6 @@ int main() {
 			break;
 		printf("Pushed %c\n", 'a'+i);
 	}
-
 	for(i = 0;; i++) {
 		c = pop(&s);
 		if (c == '$') {
@@ -86,7 +142,6 @@ int main() {
 		}
 		printf("Popped %c\n", c);
 	}
-
 	return 0;
 }
 
@@ -98,7 +153,7 @@ int main() {
 	-	Hint. User realloc function in stdlib (read the manpage for realloc)
 	-	This way the stack will get full if realloc() fails.
 */
-
+    
 
 /* Task 3.	Flexi Stack
 	-	One drawback of the Unbounded stack is that once the stack grows
@@ -107,6 +162,7 @@ int main() {
 		data when only less that half the size is in use.
 	-	Hint. User realloc function in stdlib (read the manpage for realloc)
 */
+
 
 /* Task 4.	Checking balanced parentheses 
 	-	Write a function which will take a string of opening and closing
